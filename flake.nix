3af1513501dd703e36b97c4a61378c791c95c45c
Fuxi <1538130391@qq.com>
2026-09-11 10:25:20 +0800

    chore: qqu alias in flake.nix

--- flake.nix
+++ flake.nix
@@ -19,17 +19,8 @@
       devShells.${system}.default = llvm.libcxxStdenv.mkDerivation {
         name = "qqu-dev-shell";
         nativeBuildInputs = with pkgs; [
-          (writeShellScriptBin "tests" ''
-            exec "$(git rev-parse --show-toplevel)/scripts/test" "$@"
-          '')
-          (writeShellScriptBin "bench" ''
-            exec "$(git rev-parse --show-toplevel)/scripts/benchmark" "$@"
-          '')
-          (writeShellScriptBin "plot" ''
-            exec "$(git rev-parse --show-toplevel)/scripts/plot" "$@"
-          '')
-          (writeShellScriptBin "summ" ''
-            exec "$(git rev-parse --show-toplevel)/scripts/summary" "$@"
+          (writeShellScriptBin "qqu" ''
+            exec "$(git rev-parse --show-toplevel)/scripts/.venv/bin/qqu" "$@"
           '')
           cmake
           gdb
